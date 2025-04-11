08e0e16bd7a9b52be841004d7de3a5161cf3b609
AxelleBdd <axbaudinaud@gmail.com>
2025-04-11 16:06:54 +0200

    Fin de la partie projet + page contact

--- Portfolio.js
+++ Portfolio.js
@@ -1,7 +1,47 @@
 // Déclaration des variables
 let burgerMenu = document.querySelector('header');
+let descriptionProjetUn = document.querySelector('#dialogUn');
+let descriptionProjetDeux = document.querySelector('#dialogDeux');
+let descriptionProjetTrois = document.querySelector('#dialogTrois');
+let descriptionProjetQuatre = document.querySelector('#dialogQuatre');
+let imagePremierProjet = document.querySelector('#imageProjetUn');
+let imageDeuxiemeProjet = document.querySelector('#imageProjetDeux');
+let imageTroisiemeProjet = document.querySelector('#imageProjetTrois');
+let imageQuatriemeProjet = document.querySelector('#imageProjetQuatre');
+let bouttonModaleUn = document.querySelector('#closeModaleUn');
+let bouttonModaleDeux = document.querySelector('#closeModaleDeux');
+let bouttonModaleTrois = document.querySelector('#closeModaleTrois');
+let bouttonModaleQuatre = document.querySelector('#closeModaleQuatre');
 
-// Déclaration des fonctions
+// Déclaration des event listener
 burgerMenu.addEventListener("click", () => {
     
+})
+
+imagePremierProjet.addEventListener("click", () => {
+    descriptionProjetUn.showModal();
+});
+bouttonModaleUn.addEventListener("click", () => {
+    descriptionProjetUn.close();
+})
+
+imageDeuxiemeProjet.addEventListener("click", () => {
+    descriptionProjetDeux.showModal();
+});
+bouttonModaleDeux.addEventListener("click", () => {
+    descriptionProjetDeux.close();
+})
+
+imageTroisiemeProjet.addEventListener("click", () => {
+    descriptionProjetTrois.showModal();
+});
+bouttonModaleTrois.addEventListener("click", () => {
+    descriptionProjetTrois.close();
+})
+
+imageQuatriemeProjet.addEventListener("click", () => {
+    descriptionProjetQuatre.showModal();
+});
+bouttonModaleQuatre.addEventListener("click", () => {
+    descriptionProjetQuatre.close();
 })
